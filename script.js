23f895054c5ca15ba39e34bd48fe6673655b096c
zayennn <zaayeennn@gmail.com>
2025-10-19 21:45:53 +0700

    update

--- script.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-// Based off of the Dribbble from Remi Preher
-// https://dribbble.com/shots/3927697-Ghost
-// Read the tutorial here
-// https://braydoncoyer.dev/blog/not-so-spooky-ghost-with-html-and-css/
